d80723dde67f292c6e53a87424b257b016bc9780
JangJuRi <wkdwnfl119@naver.com>
2025-01-06 19:50:28 +0900

    날짜 포맷 추가

--- src/main/java/com/study/api/post/projection/PostsWithMembers.java
+++ src/main/java/com/study/api/post/projection/PostsWithMembers.java
@@ -1,9 +1,22 @@
 package com.study.api.post.projection;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public interface PostsWithMembers {
     Long getPostId();
     String getTitle();
     String getSubTitle();
     String getContent();
+    LocalDateTime getCreatedDate();
+
+    @Value("#{target.member.name}")
     String getMemberName();
+
+    default String getFormattedCreatedDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return getCreatedDate().format(formatter);
+    }
 }
